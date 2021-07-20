6755415fa72152d6f695377f2f2f7bfbe310b613
tonin <antonio.leite00@hotmail.com>
2021-07-20 19:35:09 -0300

    Criado a estrutura do Provider e do Data Base Helper

---
+++ app/src/main/java/github/toninhobueno/contentprovider/database/NotesProvider.kt
@@ -0,0 +1,63 @@
+package github.toninhobueno.contentprovider.database
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.content.UriMatcher
+import android.database.Cursor
+import android.net.Uri
+
+class NotesProvider : ContentProvider() {
+
+    private lateinit var mUriMatcher: UriMatcher
+    override fun onCreate(): Boolean {
+        mUriMatcher = UriMatcher(UriMatcher.NO_MATCH)
+        mUriMatcher.addURI(AUTHORITY, "notes", NOTES)
+        mUriMatcher.addURI(AUTHORITY, "notes/#", NOTES_BY_ID)
+        return true
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
+        TODO("Implement this to handle requests to delete one or more rows")
+    }
+
+    override fun getType(uri: Uri): String? {
+        TODO(
+            "Implement this to handle requests for the MIME type of the data" +
+                    "at the given URI"
+        )
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        TODO("Implement this to handle requests to insert a new row.")
+    }
+
+
+    override fun query(
+        uri: Uri, projection: Array<String>?, selection: String?,
+        selectionArgs: Array<String>?, sortOrder: String?
+    ): Cursor? {
+        TODO("Implement this to handle query requests from clients.")
+    }
+
+    override fun update(
+        uri: Uri, values: ContentValues?, selection: String?,
+        selectionArgs: Array<String>?
+    ): Int {
+        TODO("Implement this to handle requests to update one or more rows.")
+    }
+
+    companion object {
+        const val AUTHORITY = "github.toninhobueno.contentprovider.provider"
+
+        val BASE_URI = Uri.parse("content://$AUTHORITY")
+        val URI_NOTES = Uri.withAppendedPath(BASE_URI,"notes")
+
+        // content://github.toninhobueno.contentprovider.provider/notes
+
+        const val NOTES = 1
+        const val NOTES_BY_ID = 2
+
+
+
+    }
+}
